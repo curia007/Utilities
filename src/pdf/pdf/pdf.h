2d6b7948a10268cf186ae17cbd4d2f0cd919e7ba
Carmelo Uria <curia@carmelouria.com>
2015-09-16 12:56:22 -0600

    Initial Creation

---
+++ src/pdf/pdf/pdf.h
@@ -0,0 +1,19 @@
+//
+//  pdf.h
+//  pdf
+//
+//  Created by Carmelo I. Uria on 9/16/15.
+//  Copyright © 2015 Carmelo I. Uria. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for pdf.
+FOUNDATION_EXPORT double pdfVersionNumber;
+
+//! Project version string for pdf.
+FOUNDATION_EXPORT const unsigned char pdfVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <pdf/PublicHeader.h>
+
+
