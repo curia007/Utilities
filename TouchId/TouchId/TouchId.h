8bc6d7c1f8626745b41b4a52b9b1ded1e7101b0e
Carmelo Uria <curia@carmelouria.com>
2015-10-02 13:43:43 -0600

    Started LocalAuthentication

---
+++ TouchId/TouchId/TouchId.h
@@ -0,0 +1,19 @@
+//
+//  TouchId.h
+//  TouchId
+//
+//  Created by Carmelo Uria on 10/2/15.
+//  Copyright © 2015 Carmelo Uria. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TouchId.
+FOUNDATION_EXPORT double TouchIdVersionNumber;
+
+//! Project version string for TouchId.
+FOUNDATION_EXPORT const unsigned char TouchIdVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TouchId/PublicHeader.h>
+
+
